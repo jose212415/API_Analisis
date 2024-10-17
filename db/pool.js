01f61e2246f225e35fe5703ad00330dee417ba6c
jose152021 <115026906+jose212415@users.noreply.github.com>
2024-10-17 07:42:53 -0700

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -2,11 +2,12 @@ const { Pool } = require('pg');
 
 //Conexion a PostgreSQL
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
+    /*user: 'postgres',
+    host: 'postgres.railway.internal',
     database: 'DB_Viajes',
-    password: '1234',
-    port: 5432,
+    password: 'wuLwhGPipNOGIWykSZJUIyCmsvFmQYMM',
+    port: 5432,*/
+    connectionString: 'postgresql://postgres:wuLwhGPipNOGIWykSZJUIyCmsvFmQYMM@junction.proxy.rlwy.net:37969/railway',
 });
 
 module.exports = pool;
